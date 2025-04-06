7cf2348ddde92ad371c085213e2a55db55d009c6
HaR-S-H <119717186+HaR-S-H@users.noreply.github.com>
2025-04-07 00:41:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(express.json());
 app.use(cors(
     {
-        origin: "http://localhost:5173",
+        origin: "https://anantgita.netlify.app",
         credentials: true,
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     }
