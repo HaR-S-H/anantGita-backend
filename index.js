2617ce6742f686dd36f4b016c6f8f453f22131ac
HaR-S-H <119717186+HaR-S-H@users.noreply.github.com>
2025-07-04 10:09:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ import quizHistoryRoute from "./routes/quizHistory.routes.js"
 dotenv.config();
 
 const app = express();
-
+app.set("trust proxy", 1);
 app.use(express.json());
 app.use(cors(
     {
